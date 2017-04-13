51cb1f210937565ef5d6137b0ab0bedb88d07f6f
venkat <nhvenkatdatta@gmail.com>
2017-04-14 00:37:38 +0530

    Added support to display Slave Page

--- assets/js/getLog.js
+++ assets/js/getLog.js
@@ -2,6 +2,8 @@ var obj = {
   xhr : new XMLHttpRequest() ,
   slaveSync : null,
   appSync : null,
+  slaveCpuSync : null,
+  slaveHost : [] ,
   getAppName : function(){
     obj.xhr.onreadystatechange = obj.appList;
     obj.xhr.open("GET" , "http://127.0.0.1:5000/status");
@@ -65,8 +67,11 @@ var obj = {
   appendSlave : function(){
     if(obj.xhr.readyState == 4 && obj.xhr.status== 200){
       var data = JSON.parse(this.responseText);
-      var elem = document.getElementById("slaves");
-      elem.innerHTML+="<div class='col-md-4'><div class='card'><div class='header'><h5 class='title'>"+data["hostname"]+"</h5><button class='btn btn-info btn-fill pull-right' onclick='window.location.href='./system.html'> View </button></div><div class='content'><div class='footer'><hr><div class='stats'><i class='fa fa-clock-o'></i> Updated 3 minutes ago</div></div></div></div></div>"
+      if(obj.slaveHost.indexOf(data["hostname"]) == -1){
+          var elem = document.getElementById("slaves");
+          elem.innerHTML+="<div class='col-md-4'><div class='card'><div class='header'><h5 class='title' id ='slave_host_name'>"+data["hostname"]+"</h5><button class='btn btn-info btn-fill pull-right' onclick='loadSlave()'> View </button></div><div class='content'><div class='footer'><hr><div class='stats'>CPU : "+data["cpus"]+"</div><hr><div class='stats'>Memory : "+data["mem"]+"</div></div></div></div></div>"
+          obj.slaveHost.push(data["hostname"]);
+      }
       obj.slaveSync = setInterval(obj.getSlaves , 10000);
     }
   },
@@ -78,10 +83,11 @@ var obj = {
   cpuSlave : function(){
     if(obj.xhr.readyState == 4 && obj.xhr.status== 200){
       var elem = document.getElementById("cpuStat");
-      alert(parseInt(this.responseText));
+      //alert(parseInt(this.responseText));
       elem.setAttribute("class" , "c100 p"+parseInt(this.responseText)+" big orange");
       var elem1 = document.getElementById("cpuVal");
       elem1.innerHTML=parseInt(this.responseText);
+      slaveCpuSync = setInterval(obj.getUtil, 10000);
     }
   },
   uploadApp : function(){
@@ -142,16 +148,58 @@ var obj = {
       elem2.setAttribute("class" , "c100 p"+parseInt(data["mem"])+" big orange");
       var elem22 = document.getElementById("memVal");
       elem22.innerHTML = parseInt(data["mem"]);
-      
+
       elem3.setAttribute("class" , "c100 p"+parseInt(data["io"])+" big orange");
       var elem33 = document.getElementById("ioVal");
       elem33.innerHTML = parseInt(data["io"]);
-       
+
     }
   }
 
+};
+
+function loadSlave(name){
+  var elem = document.getElementById("slave_host_name");
+  alert(elem.innerHTML);
+  createCookie('slave_host',elem.innerHTML,7);
+  window.location.href = "./system.html";
 }
+
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+function updateSlavePage(){
+  var host = readCookie('slave_host');
+  alert(host);
+  var elem = document.getElementById("slaveName");
+  elem.innerHTML = host;
+  obj.getUtil();
+}
+
 //obj.getData("get");
+/*
 var app_count=0;
 function addApps(appList){
   var list = appList ;
@@ -176,5 +224,6 @@ function loadPage(appName){
 }
 function appPage(){
   appDetails = obj.appDetails();
-  document.getElementById("app_name").value = 
+  //document.getElementById("app_name").value =
 }
+*/
